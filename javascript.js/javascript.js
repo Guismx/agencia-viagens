de02010d95be8cbec19e18d11ba1e8700489f516
Guilherme Rodrigues <Guilhermesmx@gmail.com>
2023-12-21 20:23:53 -0300

    add carrousel

--- javascript.js/javascript.js
+++ javascript.js/javascript.js
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-// TRANSPARÊNCIA DO MENU
+// TRANSPARÊNCIA DO MENU INDEX
 window.addEventListener("scroll", function () {
   var navbar = document.getElementById("navbar-index");
 
@@ -43,3 +43,58 @@ window.addEventListener("scroll", function () {
     navbar.classList.remove("transparent");
   }
 });
+
+// CARROSEL PACOTES INDEX/1
+
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
+
+let idx = 0;
+
+function carroselindex() {
+  idx++;
+
+  if (idx > img.length - 1) {
+    idx = 0;
+  }
+
+  imgs.style.transform = `translateX(${-idx * 280}px)`;
+}
+
+setInterval(carroselindex, 2000);
+
+// CARROSEL PACOTES INDEX/2
+const imgs2 = document.getElementById("img2");
+const img2 = document.querySelectorAll("#img2 img");
+
+let idy = 0;
+
+function carroselindex2() {
+  idy++;
+
+  if (idy > img.length - 1) {
+    idy = 0;
+  }
+
+  imgs2.style.transform = `translateX(${-idy * 280}px)`;
+}
+
+setInterval(carroselindex2, 2000);
+
+//CARROSEL PACOTES INDEX/3
+const imgs3 = document.getElementById("img3");
+const img3 = document.querySelectorAll("#img3 img");
+
+let idz = 0;
+
+function carroselindex3() {
+  idz++;
+
+  if (idz > img.length - 1) {
+    idz = 0;
+  }
+
+  imgs3.style.transform = `translateX(${-idz * 280}px)`;
+}
+
+setInterval(carroselindex3, 2000);
